067243bfa0f6f3e4936952cb619a0a92875afe9e
igonglei <gl20061041@hotmail.com>
2018-08-27 01:26:49 +0800

    template & animation

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,16 @@
 //index.js
-const { imageCDN } = require('../../config')
+const {
+  imageCDN
+} = require('../../config')
+
+const animation = {
+  startImg: 'start-img-ani',
+  startText: 'start-text-ani',
+  avatar: 'avatar-ani',
+  headTitleAni: 'head-title-ani',
+  headSplitAni: 'head-split-ani',
+  titleImgAni: 'title-img-ani'
+}
 
 //获取应用实例
 const app = getApp()
@@ -8,15 +19,28 @@ Page({
   data: {
     resume: {},
     avatar: app.globalData.avatar,
-    startImg: `${imageCDN}resume.png`,
-    profileImg: `${imageCDN}profile.png`,
-    educationImg: `${imageCDN}education.png`,
-    jobImg: `${imageCDN}job.png`,
-    skillImg: `${imageCDN}skill.png`,
-    worksImg: `${imageCDN}works.png`,
-    contactImg: `${imageCDN}contact.png`
+    image: {
+      start: `${imageCDN}resume.png`,
+      profile: `${imageCDN}profile.png`,
+      education: `${imageCDN}education.png`,
+      job: `${imageCDN}job.png`,
+      skill: `${imageCDN}skill.png`,
+      works: `${imageCDN}works.png`,
+      contact: `${imageCDN}contact.png`
+    },
+    animation: {
+      startImg: '',
+      startText: '',
+      avatar: '',
+      aboutHeadTitleAni: '',
+      aboutHeadSplitAni: '',
+      profileHeadTitleAni: '',
+      profileHeadSplitAni: '',
+      profileTitleImgAni: ''
+    },
+    current: 0
   },
-  onLoad: function () {
+  onLoad: function() {
     const resume = app.globalData.resume
     if (resume) {
       this.setData({
@@ -29,5 +53,42 @@ Page({
         })
       }
     }
+  },
+  onShow: function() {
+    this.setAnimation()
+  },
+  onHide: function() {
+    this.clearAnimation()
+  },
+  clearAnimation: function() {
+    this.setData({
+      'animation.startImg': '',
+      'animation.startText': '',
+      'animation.avatar': '',
+      'animation.aboutHeadTitleAni': '',
+      'animation.aboutHeadSplitAni': '',
+      'animation.profileHeadTitleAni': '',
+      'animation.profileHeadSplitAni': '',
+      'animation.profileTitleImgAni': ''
+    })
+  },
+  setAnimation: function() {
+    const current = this.data.current
+    this.setData({
+      'animation.startImg': current === 0 ? animation.startImg : '',
+      'animation.startText': current === 0 ? animation.startText : '',
+      'animation.avatar': current === 1 ? animation.avatar : '',
+      'animation.aboutHeadTitleAni': current === 1 ? animation.headTitleAni : '',
+      'animation.aboutHeadSplitAni': current === 1 ? animation.headSplitAni : '',
+      'animation.profileHeadTitleAni': current === 2 ? animation.headTitleAni : '',
+      'animation.profileHeadSplitAni': current === 2 ? animation.headSplitAni : '',
+      'animation.profileTitleImgAni': current === 2 ? animation.titleImgAni : ''
+    })
+  },
+  bindchange: function(e) {
+    this.setData({
+      current: e.detail.current
+    })
+    this.setAnimation()
   }
 })
