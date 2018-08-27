88b8b3771bcd997c43f8a2eac3f489fb4264ef1e
Gonglei <leigong@douyu.tv>
2018-08-27 20:38:16 +0800

    animation & profile

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,9 +7,16 @@ const animation = {
   startImg: 'start-img-ani',
   startText: 'start-text-ani',
   avatar: 'avatar-ani',
-  headTitleAni: 'head-title-ani',
-  headSplitAni: 'head-split-ani',
-  titleImgAni: 'title-img-ani'
+  avatarCircle: 'avatar-circle-ani',
+  userName: 'userinfo-name-ani',
+  userEdu: 'userinfo-edu-ani',
+  userWorkYear: 'userinfo-workYear-ani',
+  userAddress: 'userinfo-address-ani',
+  userJob: 'userinfo-job-ani',
+  headTitle: 'head-title-ani',
+  headSplit: 'head-split-ani',
+  titleImg: 'title-img-ani',
+  profileContent: 'profile-content-ani'
 }
 
 //获取应用实例
@@ -21,6 +28,7 @@ Page({
     avatar: app.globalData.avatar,
     image: {
       start: `${imageCDN}resume.png`,
+      about: `${imageCDN}about.png`,
       profile: `${imageCDN}profile.png`,
       education: `${imageCDN}education.png`,
       job: `${imageCDN}job.png`,
@@ -32,13 +40,36 @@ Page({
       startImg: '',
       startText: '',
       avatar: '',
-      aboutHeadTitleAni: '',
-      aboutHeadSplitAni: '',
-      profileHeadTitleAni: '',
-      profileHeadSplitAni: '',
-      profileTitleImgAni: ''
+      avatarCircle: '',
+      userName: '',
+      userEdu: '',
+      userWorkYear: '',
+      userAddress: '',
+      userJob: '',
+      aboutHeadTitle: '',
+      aboutHeadSplit: '',
+      aboutTitleImg: '',
+      profileHeadTitle: '',
+      profileHeadSplit: '',
+      profileTitleImg: '',
+      profileContent: '',
+      eduHeadTitle: '',
+      eduHeadSplit: '',
+      eduTitleImg: '',
+      jobHeadTitle: '',
+      jobHeadSplit: '',
+      jobTitleImg: '',
+      skillHeadTitle: '',
+      skillHeadSplit: '',
+      skillTitleImg: '',
+      worksHeadTitle: '',
+      worksHeadSplit: '',
+      worksTitleImg: '',
+      contactHeadTitle: '',
+      contactHeadSplit: '',
+      contactTitleImg: ''
     },
-    current: 0
+    current: 3
   },
   onLoad: function() {
     const resume = app.globalData.resume
@@ -58,31 +89,42 @@ Page({
     this.setAnimation()
   },
   onHide: function() {
-    this.clearAnimation()
+    this.setAnimation(-1)
   },
-  clearAnimation: function() {
-    this.setData({
-      'animation.startImg': '',
-      'animation.startText': '',
-      'animation.avatar': '',
-      'animation.aboutHeadTitleAni': '',
-      'animation.aboutHeadSplitAni': '',
-      'animation.profileHeadTitleAni': '',
-      'animation.profileHeadSplitAni': '',
-      'animation.profileTitleImgAni': ''
-    })
-  },
-  setAnimation: function() {
-    const current = this.data.current
+  setAnimation: function(current) {
+    current = current || this.data.current
     this.setData({
       'animation.startImg': current === 0 ? animation.startImg : '',
       'animation.startText': current === 0 ? animation.startText : '',
       'animation.avatar': current === 1 ? animation.avatar : '',
-      'animation.aboutHeadTitleAni': current === 1 ? animation.headTitleAni : '',
-      'animation.aboutHeadSplitAni': current === 1 ? animation.headSplitAni : '',
-      'animation.profileHeadTitleAni': current === 2 ? animation.headTitleAni : '',
-      'animation.profileHeadSplitAni': current === 2 ? animation.headSplitAni : '',
-      'animation.profileTitleImgAni': current === 2 ? animation.titleImgAni : ''
+      'animation.avatarCircle': current === 1 ? animation.avatarCircle : '',
+      'animation.userName': current === 1 ? animation.userName : '',
+      'animation.userEdu': current === 1 ? animation.userEdu : '',
+      'animation.userWorkYear': current === 1 ? animation.userWorkYear : '',
+      'animation.userAddress': current === 1 ? animation.userAddress : '',
+      'animation.userJob': current === 1 ? animation.userJob : '',
+      'animation.aboutHeadTitle': current === 1 ? animation.headTitle : '',
+      'animation.aboutHeadSplit': current === 1 ? animation.headSplit : '',
+      'animation.aboutTitleImg': current === 1 ? animation.titleImg : '',
+      'animation.profileHeadTitle': current === 2 ? animation.headTitle : '',
+      'animation.profileHeadSplit': current === 2 ? animation.headSplit : '',
+      'animation.profileTitleImg': current === 2 ? animation.titleImg : '',
+      'animation.profileContent': current === 2 ? animation.profileContent : '',
+      'animation.eduHeadTitle': current === 3 ? animation.headTitle : '',
+      'animation.eduHeadSplit': current === 3 ? animation.headSplit : '',
+      'animation.eduTitleImg': current === 3 ? animation.titleImg : '',
+      'animation.jobHeadTitle': current === 4 ? animation.headTitle : '',
+      'animation.jobHeadSplit': current === 4 ? animation.headSplit : '',
+      'animation.jobTitleImg': current === 4 ? animation.titleImg : '',
+      'animation.skillHeadTitle': current === 5 ? animation.headTitle : '',
+      'animation.skillHeadSplit': current === 5 ? animation.headSplit : '',
+      'animation.skillTitleImg': current === 5? animation.titleImg : '',
+      'animation.worksHeadTitle': current === 6 ? animation.headTitle : '',
+      'animation.worksHeadSplit': current === 6 ? animation.headSplit : '',
+      'animation.worksTitleImg': current === 6 ? animation.titleImg : '',
+      'animation.contactHeadTitle': current === 7 ? animation.headTitle : '',
+      'animation.contactHeadSplit': current === 7 ? animation.headSplit : '',
+      'animation.contactTitleImg': current === 7 ? animation.titleImg : ''
     })
   },
   bindchange: function(e) {
