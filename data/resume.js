46267f28b2d6a2adc9ee874fb13508d9c284ffa7
Gonglei <leigong@douyu.tv>
2018-08-28 20:57:48 +0800

    education

--- data/resume.js
+++ data/resume.js
@@ -7,11 +7,13 @@ module.exports = {
   "intro": ["精通jQuery开发，熟悉Bootstrap和Vue.js等前端框架开发，熟悉.Net和Nodejs后台开发，熟悉MongoDB和Oracle等数据库开发，熟悉Nginx部署和Git使用。", "本人性格沉稳，待人热情；工作认真负责，积极主动，能吃苦耐劳；学习能力强，勇于创新。"],
   "education": [{
       "school": "石家庄铁道大学",
-      "period": "2006.09-2010.06"
+      "period": "2006.09-2010.06",
+      "degree": "本科"
     },
     {
       "school": "洪湖市第二中学",
-      "period": "2003.09-2006.06"
+      "period": "2003.09-2006.06",
+      "degree": "高中"
     }
   ]
 }
