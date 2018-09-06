286f4365134e63177bce3d8fdc464563777be453
Gonglei <leigong@douyu.tv>
2018-09-06 11:29:58 +0800

    change file host

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //配置文件
 module.exports = {
-  fileHost: 'https://gitlab.com/igonglei/pub/raw/master/static/',
+  fileHost: 'https://gitee.com/igonglei/pub/raw/master/static/',
   imageCDN: 'https://lg-npha00ki-1257320081.cos.ap-shanghai.myqcloud.com/',
   localMode: false
 }
