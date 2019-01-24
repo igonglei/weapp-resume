84e7e9f9125a0fb4736ff20c67f176a96d6a291c
igonglei <gl20061041@hotmail.com>
2019-01-25 01:27:05 +0800

    update resume data

--- data/resume.js
+++ data/resume.js
@@ -3,47 +3,60 @@ module.exports = {
   "bg": "bg.jpg",
   "startPage": 0,
   "pages": [{
-    "name": "cover"
-  }, {
-    "name": "about",
-    "title": "ABOUT",
-    "titleImage": "about.png"
-  }, {
-    "name": "profile",
-    "title": "PROFILE",
-    "titleImage": "profile.png"
-  }, {
-    "name": "education",
-    "title": "EDUCATION",
-    "titleImage": "education.png"
-  }, {
-    "name": "job",
-    "title": "JOB",
-    "titleImage": "job.png"
-  }, {
-    "name": "project",
-    "title": "PROJECT",
-    "titleImage": "project.png"
-  }, {
-    "name": "skill",
-    "title": "SKILL",
-    "titleImage": "skill.png"
-  }, {
-    "name": "works",
-    "title": "WORKS",
-    "titleImage": "works.png"
-  }, {
-    "name": "works1",
-    "title": "WORKS",
-    "titleImage": "works.png"
-  }, {
-    "name": "contact",
-    "title": "CONTACT",
-    "titleImage": "contact.png"
-  }],
+      "name": "cover"
+    },
+    {
+      "name": "about",
+      "title": "ABOUT",
+      "titleImage": "about.png"
+    },
+    {
+      "name": "profile",
+      "title": "PROFILE",
+      "titleImage": "profile.png"
+    },
+    {
+      "name": "education",
+      "title": "EDUCATION",
+      "titleImage": "education.png"
+    },
+    {
+      "name": "job",
+      "title": "JOB",
+      "titleImage": "job.png"
+    },
+    {
+      "name": "project",
+      "title": "PROJECT",
+      "titleImage": "project.png"
+    },
+    {
+      "name": "skill",
+      "title": "SKILL",
+      "titleImage": "skill.png"
+    },
+    {
+      "name": "works",
+      "title": "WORKS",
+      "titleImage": "works.png"
+    },
+    {
+      "name": "works1",
+      "title": "WORKS",
+      "titleImage": "works.png"
+    },
+    {
+      "name": "contact",
+      "title": "CONTACT",
+      "titleImage": "contact.png"
+    }
+  ],
   "shareImage": "share.jpg",
   "cover": "resume.png",
-  "slogan": ["放飞梦想", "扬帆起航"],
+  "slogan": [
+    "放飞梦想",
+    "扬帆起航"
+  ],
   "avatar": "gonglei.jpg",
   "name": "龚磊",
   "job": "前端工程师",
@@ -52,90 +65,115 @@ module.exports = {
   "edu": "本科",
   "phone": "13100600034",
   "qrcode": "qrcode.jpg",
-  "intro": ["精通HTML5/CSS3/JQUERY，熟悉React/Vue/Bootstrap等前端框架，熟悉.Net/Nodejs等后端开发，熟悉MongoDB/MySQL等数据库，熟悉Linux、Nginx、Git的使用。", "本人性格沉稳，待人真诚；工作认真负责，积极主动，能吃苦耐劳；学习能力强，勇于创新。"],
+  "intro": [
+    "精通HTML5/CSS3/JQUERY，熟悉React/Vue等前端框架，熟悉.Net/Nodejs等后端开发，熟悉MongoDB/MySQL等数据库开发，熟悉微信小程序开发，熟悉Linux/Nginx/Git的使用。",
+    "本人性格沉稳，待人真诚；工作认真负责，积极主动，能吃苦耐劳；学习能力强，勇于创新。"
+  ],
   "education": [{
-    "period": "2006.09 - 2010.06",
-    "info": "石家庄铁道大学 本科"
-  }, {
-    "period": "2003.09 - 2006.06",
-    "info": "洪湖市第二中学 高中"
-  }],
+      "period": "2006.09 - 2010.06",
+      "info": "石家庄铁道大学 本科"
+    },
+    {
+      "period": "2003.09 - 2006.06",
+      "info": "洪湖市第二中学 高中"
+    }
+  ],
   "jobs": [{
-    "period": "2018.08 - 2019.01",
-    "info": "武汉斗鱼网络科技有限公司"
-  }, {
-    "period": "2014.08 - 2018.08",
-    "info": "软通动力技术服务有限公司"
-  }, {
-    "period": "2011.04 - 2014.08",
-    "info": "北京尖峰计算机系统有限公司"
-  }],
+      "period": "2018.08 - 2019.01",
+      "info": "武汉斗鱼网络科技有限公司"
+    },
+    {
+      "period": "2014.08 - 2018.08",
+      "info": "软通动力技术服务有限公司"
+    },
+    {
+      "period": "2011.04 - 2014.08",
+      "info": "北京尖峰计算机系统有限公司"
+    }
+  ],
   "projects": [{
-    "period": "2018.11 - 2018.11",
-    "info": "斗鱼极速版激励体系",
-    "desc": "移动端，拉新激励"
-  }, {
-    "period": "2017.04 - 2018.05",
-    "info": "华为武研OWL运维系统",
-    "desc": "大屏可视化，2D实验室"
-  }, {
-    "period": "2016.05 - 2017.01",
-    "info": "华为网络SDE运维系统",
-    "desc": "大屏可视化，多维度统计"
-  }, {
-    "period": "2013.04 - 2014.05",
-    "info": "中体彩数据仓库系统",
-    "desc": "数据抓取，实时监控"
-  }],
+      "period": "2018.11 - 2018.11",
+      "info": "斗鱼极速版激励体系",
+      "desc": "移动端，React"
+    },
+    {
+      "period": "2017.04 - 2018.05",
+      "info": "华为武研OWL运维系统",
+      "desc": "大屏可视化，2D实验室"
+    },
+    {
+      "period": "2016.05 - 2017.01",
+      "info": "华为网络SDE运维系统",
+      "desc": "大屏可视化，多维度统计"
+    },
+    {
+      "period": "2013.04 - 2014.05",
+      "info": "中体彩数据仓库系统",
+      "desc": "数据抓取，实时监控"
+    }
+  ],
   "skills": [{
-    "text": "HTML5",
-    "value": 80
-  }, {
-    "text": "CSS3",
-    "value": 80
-  }, {
-    "text": "REACT",
-    "value": 70
-  }, {
-    "text": "VUE",
-    "value": 70
-  }, {
-    "text": "JQUERY",
-    "value": 80
-  }, {
-    "text": "BOOTSTRAP",
-    "value": 70
-  }, {
-    "text": "NODEJS",
-    "value": 60
-  }],
+      "text": "HTML5",
+      "value": 80
+    },
+    {
+      "text": "CSS3",
+      "value": 80
+    },
+    {
+      "text": "REACT",
+      "value": 70
+    },
+    {
+      "text": "VUE",
+      "value": 70
+    },
+    {
+      "text": "JQUERY",
+      "value": 80
+    },
+    {
+      "text": "NODEJS",
+      "value": 60
+    },
+    {
+      "text": "微信小程序",
+      "value": 60
+    }
+  ],
   "works": [{
-    "cover": "sde-home.cover.png",
-    "src": "sde-home.png"
-  }, {
-    "cover": "sde-order.cover.png",
-    "src": "sde-order.png"
-  }, {
-    "cover": "owl-uti.cover.png",
-    "src": "owl-uti.png"
-  }],
+      "cover": "wk.enveditor.cover.png",
+      "src": "wk.enveditor.png"
+    },
+    {
+      "cover": "wk.lab.cover.png",
+      "src": "wk.lab.png"
+    },
+    {
+      "cover": "wk.topo.cover.png",
+      "src": "wk.topo.png"
+    }
+  ],
   "works1": [{
-    "cover": "enveditor.cover.png",
-    "src": "enveditor.png"
-  }, {
-    "cover": "voice-queen.cover.png",
-    "src": "voice-queen.png"
-  }, {
-    "cover": "douyu-invite.cover.png",
-    "src": "douyu-invite.png"
-  }],
+      "cover": "wk.douyu.voice-queen.cover.png",
+      "src": "wk.douyu.voice-queen.png"
+    },
+    {
+      "cover": "wk.douyu.invite-share.cover.png",
+      "src": "wk.douyu.invite-share.png"
+    },
+    {
+      "cover": "wk.douyu.pubg-barrage.cover.png",
+      "src": "wk.douyu.pubg-barrage.png"
+    }
+  ],
   "contact": [{
-      "text": "EMAIL",
+      "text": "邮箱",
       "value": "gl20061041@hotmail.com"
     },
     {
-      "text": "GITHUB",
-      "value": "https://github.com/igonglei"
+      "text": "主页",
+      "value": "https://igonglei.gitee.io/clean-ui"
     }
   ],
   "callText": "一键拨号"
